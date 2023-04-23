c00de5d68a9d52ddc15d7867a8fd8bb9615ec527
hermtechs <hermtechs@gmail.com>
2023-04-23 19:09:13 +0300

    calculator app with react.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import './styles.css';
+function App(){
+  return(
+  <div className="grid-container">
+   <div className="input-output-wrapper">
+    <div className="prev-operand"></div>
+    <div className="current-operand"></div>
+    </div> 
+
+    <button className="span-2">AC</button>
+    <button>DEL</button>
+    <button>÷</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>*</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>+</button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button>-</button>
+    <button>.</button>
+    <button>0</button>
+    <button className="span-2">=</button>
+    
+
+  </div>)
 }
 
 export default App;
